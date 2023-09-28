1053521e28cdf7e3977dae898ac01e38680b3e88
Vinay Kandoi <vinkandoi@gmail.com>
2023-09-28 21:01:14 +0530

    Created Cards Card form and Nav Menu

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const btnNewCard=document.getElementById("new-card");
+const cardEdit=document.querySelector(".card-edit");
+
+let toggleFlag=false;
+
+btnNewCard.addEventListener('click', event=>{
+    toggleFlag=!toggleFlag;
+    cardEdit.style.display=toggleFlag?"flex":"none";
+    
+});
+
+
+
